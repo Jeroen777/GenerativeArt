aec5cd454c5c3d75f175b88ce4798fa6e797f952
Jeroen777 <jeroenbesselink98@gmail.com>
2021-05-19 20:10:35 +0200

    small changes

--- sketch.js
+++ sketch.js
@@ -1,32 +1,36 @@
-let points = []
-let mult = 0.005
-let t = 0;
-let wow = 0;
+let marks = []
+
+let more = 0.005
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  background(0)
+  background(0);
   angleMode(DEGREES)
 
   frameRate(60);
 
+  textSize(32);
+  text('change color', 10, 30);
+  fill(255, 102, 153);
   slider1 = createSlider(0, 255, 100);
   slider1.position(10, 10);
 
-	slider2 = createSlider(0, 255, 0);
+  slider2 = createSlider(0, 255, 0);
   slider2.position(10, 30);
 
-	slider3 = createSlider(0, 255, 255);
+  slider3 = createSlider(0, 255, 255);
   slider3.position(10, 50);
 
+  slider4 = createSlider(90, 100, 0);
+  slider4.position(10, 130);
 
-  let density = 50;
   let space = 20;
 
   for (let x = 0; x < width; x += space) {
     for (let y = 0; y < height; y += space) {
       let p = createVector(x, y)
-      points.push(p)
+      marks.push(p)
     }
   }
 }
@@ -34,24 +38,27 @@ function setup() {
 function draw() {
 
   fill(255);
-  
-    let r = slider1.value();
-    let g = slider2.value();
-    let b = slider3.value();
 
-    noStroke();
-    fill( r, g, b);
+  let t = slider1.value();
 
-  for (let i = 0; i < points.length; i++) {
+  let r = slider1.value();
+  let g = slider2.value();
+  let b = slider3.value();
 
-    let fun = rotate(PI / noise(t + 2));
-    
-    let angle = map(noise(points[i].x * mult, points[i].y * mult), 0, 1, 0, 720)
+  noStroke();
+  fill(r, g, b);
 
-    points[i].add(createVector(cos(angle), sin(angle)))
+  for (let i = 0; i < marks.length; i++) {
 
-    ellipse(points[i].x, points[i].y, 0.5)
+    let fun = rotate(PI / noise(t + 1));
+
+    let angle = map(noise(marks[i].x * more, marks[i].y * more), 0, 1, 0, 720)
+
+    marks[i].add(createVector(cos(angle), sin(angle)))
+
+    ellipse(marks[i].x, marks[i].y, 0.5)
   }
+
 
 
 }
